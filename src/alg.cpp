2627641794f447092255677e5cf78849736ad8ad
gllyforg <shilovaksenia2018@mail.ru>
2026-04-17 23:05:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,105 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
+int getPriority(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
+}
+bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char, 100> stack;
+    std::string result;
+    for (size_t i = 0; i < inf.length(); ++i) {
+        char c = inf[i];
+        if (c == ' ') {
+            continue;
+        }
+        if (isdigit(c)) {
+            while (i < inf.length() && isdigit(inf[i])) {
+                result += inf[i];
+                ++i;
+            }
+            result += ' ';
+            --i;
+        }
+        else if (c == '(') {
+            stack.push(c);
+        }
+        else if (c == ')') {
+            while (!stack.isEmpty() && stack.top() != '(') {
+                result += stack.pop();
+                result += ' ';
+            }
+            if (!stack.isEmpty() && stack.top() == '(') {
+                stack.pop();
+            }
+        }
+        else if (isOperator(c)) {
+            while (!stack.isEmpty() && stack.top() != '(' && 
+                   getPriority(stack.top()) >= getPriority(c)) {
+                result += stack.pop();
+                result += ' ';
+            }
+            stack.push(c);
+        }
+    }
+    while (!stack.isEmpty()) {
+        result += stack.pop();
+        result += ' ';
+    }
+    if (!result.empty() && result.back() == ' ') {
+        result.pop_back();
+    }
+  return result;
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> stack;
+    for (size_t i = 0; i < post.length(); ++i) {
+        char c = post[i];
+        if (c == ' ') {
+            continue;
+        }
+        if (isdigit(c)) {
+            int number = 0;
+            while (i < post.length() && isdigit(post[i])) {
+                number = number * 10 + (post[i] - '0');
+                ++i;
+            }
+            stack.push(number);
+            --i;
+        }
+        else if (isOperator(c)) {
+            int b = stack.pop();
+            int a = stack.pop();
+            int result = 0;
+            switch (c) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+            }
+            
+            stack.push(result);
+        }
+    }
+  return stack.pop();
 }
